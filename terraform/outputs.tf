4ddb5422cb20016650d1bd444da474ab363a8e07
Andrew <begineconst@gmail.com>
2025-03-17 20:47:46 +0200

    Setup database replication using Ansible

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,15 @@
-output "instance_ips" {
-  value = aws_instance.terraform-server[*].public_ip
+output "master" {
+  value = {
+    public_ip  = aws_instance.terraform-server[0].public_ip
+    private_ip = aws_instance.terraform-server[0].private_ip
+    public_dns = aws_instance.terraform-server[0].public_dns
+  }
+}
+
+output "slave" {
+  value = {
+    public_ip  = aws_instance.terraform-server[1].public_ip
+    private_ip = aws_instance.terraform-server[1].private_ip
+    public_dns = aws_instance.terraform-server[1].public_dns
+  }
 }
